1f128f2e84cea19f55531898d8a564f6e6a4a2cb
Danny Van Stemp <danwardvs@adsgames.net>
2015-08-17 21:23:41 -0400

    Beep upgraded to change length

--- simplebeep.py
+++ simplebeep.py
@@ -7,27 +7,48 @@ PyAudio = pyaudio.PyAudio
 #See http://en.wikipedia.org/wiki/Bit_rate#Audio
 BITRATE = 16000 #number of frames per second/frameset.      
 
-#See http://www.phy.mtu.edu/~suits/notefreqs.html
-FREQUENCY = 261.63 #Hz, waves per second, 261.63=C4-note.
-LENGTH = 1.2232 #seconds to play sound
 
-NUMBEROFFRAMES = int(BITRATE * LENGTH)
-RESTFRAMES = NUMBEROFFRAMES % BITRATE
-WAVEDATA = ''    
 
-for x in xrange(NUMBEROFFRAMES):
- WAVEDATA = WAVEDATA+chr(int(math.sin(x/((BITRATE/FREQUENCY)/math.pi))*127+128))    
 
-#fill remainder of frameset with silence
-for x in xrange(RESTFRAMES): 
- WAVEDATA = WAVEDATA+chr(128)
 
-p = PyAudio()
-stream = p.open(format = p.get_format_from_width(1), 
+
+def beep(LENGTH):
+    #See http://www.phy.mtu.edu/~suits/notefreqs.html
+    FREQUENCY = 900 #Hz, waves per second, 261.63=C4-note.
+
+
+    NUMBEROFFRAMES = int(BITRATE * LENGTH)
+    RESTFRAMES = NUMBEROFFRAMES % BITRATE
+    WAVEDATA = ''    
+
+    for x in xrange(NUMBEROFFRAMES):
+     WAVEDATA = WAVEDATA+chr(int(math.sin(x/((BITRATE/FREQUENCY)/math.pi))*127+128))    
+
+    #fill remainder of frameset with silence
+    for x in xrange(RESTFRAMES): 
+     WAVEDATA = WAVEDATA+chr(128)
+
+    p = PyAudio()
+
+
+
+    stream = p.open(format = p.get_format_from_width(1), 
                 channels = 1, 
                 rate = BITRATE, 
                 output = True)
-stream.write(WAVEDATA)
-stream.stop_stream()
-stream.close()
-p.terminate()
+    
+    stream.write(WAVEDATA)
+
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+
+beep(0.3)
+beep(0.3)
+beep(0.7)
+beep(0.7)
+
+
+
+
