f0a3d5879d61419563e23d44f0683a636cf59fc9
Danny Van Stemp <danwardvs@adsgames.net>
2015-08-17 20:27:45 -0400

    Better beep from below it on stackoverflow

--- simplebeep.py
+++ simplebeep.py
@@ -1,20 +1,33 @@
 import math
 import pyaudio
-import sys
 
+#sudo apt-get install python-pyaudio
 PyAudio = pyaudio.PyAudio
-RATE = 16000
-WAVE = 1000
-data = ''.join([chr(int(math.sin(x/((RATE/WAVE)/math.pi))*127+128)) for x in xrange(RATE)])
-p = PyAudio()
 
-stream = p.open(format =
-                p.get_format_from_width(1),
-                channels = 1,
-                rate = RATE,
+#See http://en.wikipedia.org/wiki/Bit_rate#Audio
+BITRATE = 16000 #number of frames per second/frameset.      
+
+#See http://www.phy.mtu.edu/~suits/notefreqs.html
+FREQUENCY = 261.63 #Hz, waves per second, 261.63=C4-note.
+LENGTH = 1.2232 #seconds to play sound
+
+NUMBEROFFRAMES = int(BITRATE * LENGTH)
+RESTFRAMES = NUMBEROFFRAMES % BITRATE
+WAVEDATA = ''    
+
+for x in xrange(NUMBEROFFRAMES):
+ WAVEDATA = WAVEDATA+chr(int(math.sin(x/((BITRATE/FREQUENCY)/math.pi))*127+128))    
+
+#fill remainder of frameset with silence
+for x in xrange(RESTFRAMES): 
+ WAVEDATA = WAVEDATA+chr(128)
+
+p = PyAudio()
+stream = p.open(format = p.get_format_from_width(1), 
+                channels = 1, 
+                rate = BITRATE, 
                 output = True)
-for DISCARD in xrange(5):
-    stream.write(data)
+stream.write(WAVEDATA)
 stream.stop_stream()
 stream.close()
 p.terminate()
